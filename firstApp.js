8e90276b67e66f8ca7db2bdffe4764b74d5d3d89
tmontanaro <teodoro.montanaro@unisalento.it>
2021-04-07 10:21:29 +0200

    add server contact

--- firstApp.js
+++ firstApp.js
@@ -1,4 +1,5 @@
-var sensorLib = require('node-dht-sensor');
+const sensorLib = require('node-dht-sensor');
+const http = require('http')
 
 // Setup sensor, exit if failed
 var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
@@ -13,4 +14,40 @@ setInterval(function() {
     var readout = sensorLib.read();
     console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
     console.log('Humidity:   ', readout.humidity.toFixed(1)    + '%');
+
+    var temperature = readout.temperature.toFixed(1);
+    const data = JSON.stringify({
+        "sensor":"ID1",
+        "timestamp": 123456789
+        "temperature":20
+    })
+
+    const options = {
+        hostname: '192.168.1.251',
+        port: 3000,
+        path: '/temperature',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    }
+
+    const req = http.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`);
+        res.on('data', d => {
+            process.stdout.write(d)
+        });
+        req.on('error', error => {
+            console.error(error)
+        });
+
+    })
+
+    req.write(data)
+    req.end()
 }, 2000);
+
+
+
+
